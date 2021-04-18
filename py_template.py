52f01239197dff078cd3400bb2b509c410b9bb70
bliutwo <bliutwo@cs.stanford.edu>
2021-04-17 21:55:47 -0700

    Add specs as docstring to functions

--- py_template.py
+++ py_template.py
@@ -15,9 +15,28 @@ def get_input_from_file_as_single_string(filename: str) -> str:
     return s
 
 def find_and_replace(text: str, query: str, replace: str) -> str:
+    """
+    Write a function `find_and_replace()` that takes three arguments:
+
+    - a string text *t*, which is the text used to search and replace
+    - a string variable *query*, which is the query to find
+    - a string variable *replace*, which is what you should replace every instance of *query* with
+
+    Output: modified *t* with all instances of *query* replaced with *replace*
+    """
     return ""
 
 def find_and_replace_regex(text: str, reg_expr_str: str) -> str:
+    """
+    Write a function `find_and_replace_regex()` that takes two arguments:
+
+    - a string text *t*
+    - a regular expression string *r*
+
+    For every instance of text that matches the regular expression *r*, put two asterisks on each side of it.
+
+    Output: *t* with modifications as specified
+    """
     return ""
 
 def main():
