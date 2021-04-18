746b6edba00d563b3ac3303a1e2e00368cb59187
bliutwo <bliutwo@cs.stanford.edu>
2021-04-17 22:02:33 -0700

    Add example of built-in .replace

--- py_template.py
+++ py_template.py
@@ -1,11 +1,13 @@
 from typing import List
 
+
 def get_input_from_file_as_list_of_lines(filename: str) -> List[str]:
     l = []
     with open(filename) as f:
         for line in f:
             l.append(line)
     return l
+
 
 def get_input_from_file_as_single_string(filename: str) -> str:
     s = ""
@@ -14,17 +16,11 @@ def get_input_from_file_as_single_string(filename: str) -> str:
             s += line
     return s
 
-def find_and_replace(text: str, query: str, replace: str) -> str:
-    """
-    Write a function `find_and_replace()` that takes three arguments:
 
-    - a string text *t*, which is the text used to search and replace
-    - a string variable *query*, which is the query to find
-    - a string variable *replace*, which is what you should replace every instance of *query* with
+# OK, turns out this was actually just trivial - library exists
+# def find_and_replace(text: str, old: str, new: str) -> str:
+#     return text.replace(old, new)
 
-    Output: modified *t* with all instances of *query* replaced with *replace*
-    """
-    return ""
 
 def find_and_replace_regex(text: str, reg_expr_str: str) -> str:
     """
@@ -42,8 +38,11 @@ def find_and_replace_regex(text: str, reg_expr_str: str) -> str:
 def main():
     print("Hello world!")
     filename = "lorem_ipsum.txt"
-    print(get_input_from_file_as_list_of_lines(filename))
-    print(get_input_from_file_as_single_string(filename))
+    # print(get_input_from_file_as_list_of_lines(filename))
+    # print(get_input_from_file_as_single_string(filename))
+    t = get_input_from_file_as_single_string(filename)
+    print(t.replace('ipsum', 'asdfghhljsdklfjkafjoiewajsajkbja'))
+
 
 if __name__ == "__main__":
     main()
