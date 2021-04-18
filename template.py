06c2188883ea121d565aa3e013ed361d46291af8
bliutwo <bliutwo@cs.stanford.edu>
2021-04-17 17:11:18 -0700

    Support file input as single string

--- template.py
+++ template.py
@@ -7,10 +7,18 @@ def get_input_from_file(filename: str) -> List[str]:
             l.append(line)
     return l
 
+def get_input_from_file_as_single_string(filename: str) -> str:
+    s = ""
+    with open(filename) as f:
+        for line in f:
+            s += line
+    return s
+
 def main():
     print("Hello world!")
     filename = "patients.csv"
     print(get_input_from_file(filename))
+    print(get_input_from_file_as_single_string(filename))
 
 if __name__ == "__main__":
     main()
